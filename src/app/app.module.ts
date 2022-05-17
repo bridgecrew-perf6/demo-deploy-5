72eac1b719165f5ddf12ef6aa65fcdab64dfbd14
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-17 11:00:25 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+
+import { GithubFollowersService } from './github-followers/github-followers.service';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GithubFollowersComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'followers', component: GithubFollowersComponent }
+    ])
   ],
-  providers: [],
+  providers: [GithubFollowersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
